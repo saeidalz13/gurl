e286c2099c1563b9f97ae075cdb174c44ada9c88
Saeid Alizadeh <saeidalz96@gmail.com>
2024-10-19 10:12:57 -0600

    modified stringutils internal package

--- internal/stringutils/string_utils.go
+++ internal/stringutils/string_utils.go
@@ -15,6 +15,7 @@ func SplitDomainIntoSegments(domain string) ([]string, error) {
 }
 
 func TrimDomainPrefix(domain string) (string, error) {
+	domain = strings.TrimSpace(domain)
 	domain = strings.TrimPrefix(domain, "http://")
 	domain = strings.TrimPrefix(domain, "https://")
 	domain = strings.TrimPrefix(domain, "www.")
@@ -24,4 +25,16 @@ func TrimDomainPrefix(domain string) (string, error) {
 	}
 
 	return domain, nil
+}
+
+// Returns domain and path from the raw domain 
+// provided by user.
+func ExtractPathFromDomain(domain string) (string, string) {
+	segments := strings.SplitN(domain, "/", 2)
+
+	if len(segments) == 1 || segments[1] == "" {
+		return segments[0], "/"
+	}
+
+	return segments[0], "/" + segments[1]
 }
