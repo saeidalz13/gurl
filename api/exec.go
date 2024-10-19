130b062355e4914f7bd1976019bd14bf2a54407b
Saeid Alizadeh <saeidalz96@gmail.com>
2024-10-19 10:15:00 -0600

    added http req builder; modified funcs

--- api/exec.go
+++ api/exec.go
@@ -1,28 +1,55 @@
 package api
 
 import (
-	"fmt"
-
-	"github.com/saeidalz13/gurl/internal/httpconstants"
+	"strings"
 )
 
-type gurlCli struct {
-	domain string
-	method string
+func createHTTPRequest(gp gurlParams) string {
+	sb := strings.Builder{}
+	sb.Grow(50)
+
+	// Method
+	sb.WriteString(gp.method)
+	sb.WriteString(" ")
+
+	// Path
+	sb.WriteString(gp.path)
+	sb.WriteString(" ")
+
+	// Protocol and version
+	sb.WriteString("HTTP/1.1\r\n")
+
+	// Host
+	sb.WriteString("Host: ")
+	sb.WriteString(gp.domain)
+	sb.WriteString("\r\n")
+
+	// User-Agent
+	sb.WriteString("User-Agent: gurl/1.0.0\r\n")
+
+	// Accept headers
+	sb.WriteString("Accept: */*\r\n")
+
+	// User Headers
+	for _, header := range gp.headers {
+		sb.WriteString(header)
+		sb.WriteString("\r\n")
+	}
+
+	// Ending of request based on HTTP
+	sb.WriteString("\r\n")
+
+	return sb.String()
 }
 
 func ExecGurl() {
-	gc := initGurlCli()
-	ip := mustFetchDomainIp(gc.domain)
+	gp := initGurlCli()
+	ip := mustFetchDomainIp(gp.domain)
 
-	tlsConn := initTlsConn(ip.String(), gc.domain)
+	tlsConn := initTlsConn(ip.String(), gp.domain)
 	defer tlsConn.Close()
 
-	switch gc.method {
-	case httpconstants.MethodGET:
-		execGetHttpReq(tlsConn, gc.domain)
+	httpRequest := createHTTPRequest(gp)
 
-	case httpconstants.MethodPOST:
-		fmt.Println("Posting...")
-	}
+	execHTTPReq(tlsConn, httpRequest)
 }
--- api/http_req.go
+++ api/http_req.go
@@ -3,36 +3,14 @@ package api
 import (
 	"crypto/tls"
 	"fmt"
-	"os"
-	"strings"
 )
 
-func execGetHttpReq(tlsConn *tls.Conn, domain string) {
-	for {
-		_, err := tlsConn.Write([]byte("GET / HTTP/1.1\r\n" +
-			"Host: " + domain + "\r\nUser-Agent: Client\r\nAccept: */*\r\nConnection: close\r\n\r\n"))
-
-		if err != nil {
-			fmt.Printf("error write: %v\n", err)
-			break
-		}
-
-		buf := make([]byte, 2<<12)
-		n, err := tlsConn.Read(buf)
-		if err != nil {
-			if err.Error() == "EOF" {
-				os.Exit(0)
-			}
-
-			if strings.Contains(err.Error(), "i/o timeout") {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			fmt.Printf("error read: %v\n", err)
-			continue
-		}
-
-		fmt.Println(string(buf[:n]))
+func execHTTPReq(tlsConn *tls.Conn, httpRequest string) {
+	if err := writeToTLSConn(tlsConn, []byte(httpRequest)); err != nil {
+		fmt.Printf("write tcp read: %v\n", err)
+		return
 	}
+
+	readBytes := readFromTLSConn(tlsConn)
+	fmt.Println(string(readBytes))
 }
