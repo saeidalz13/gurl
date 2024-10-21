9771a80a5bf609ac4f96fb33ab3ed80cc9947f80
Saeid Alizadeh <saeidalz96@gmail.com>
2024-10-21 10:31:43 -0600

    added http response body parser

--- api/http_req.go
+++ api/http_req.go
@@ -3,7 +3,63 @@ package api
 import (
 	"crypto/tls"
 	"fmt"
+	"strings"
 )
+
+/*
+HTTP response includes 3 main segments
+ 1. Status line
+ 2. Headers (variable number of lines)
+ 3. Body (Separated from the rest with \r\n)
+*/
+type HTTPResponse struct {
+	version    string
+	statusCode string
+	statusMsg  string
+	headers    []string
+	body       string
+}
+
+func newHTTPResponse(response string) HTTPResponse {
+	responseSegments := strings.Split(response, "\r\n")
+	httpResp := HTTPResponse{
+		headers: make([]string, 0, 3),
+	}
+
+	var bodyIdx int
+	for i, segment := range responseSegments {
+		// First line is always the status line
+		if i == 0 {
+			statusLineSegments := strings.Split(segment, " ")
+			httpResp.version = statusLineSegments[0]
+			httpResp.statusCode = statusLineSegments[1]
+			httpResp.statusMsg = statusLineSegments[2]
+			continue
+		}
+
+		// When separating by \r\n, the empty line would
+		// would be the separator between header and body
+		if segment == "" {
+			bodyIdx = i + 1
+			break
+		}
+
+		httpResp.headers = append(httpResp.headers, segment)
+	}
+
+	sb := strings.Builder{}
+	sb.Grow(10) // 10 is min length of body
+
+	// We found the idx that starts the body
+	// aggregate again to have body string
+	for _, seg := range responseSegments[bodyIdx:] {
+		sb.WriteString(seg)
+	}
+
+	httpResp.body = sb.String()
+
+	return httpResp
+}
 
 func execHTTPReq(tlsConn *tls.Conn, httpRequest string) {
 	if err := writeToTLSConn(tlsConn, []byte(httpRequest)); err != nil {
@@ -12,5 +68,7 @@ func execHTTPReq(tlsConn *tls.Conn, httpRequest string) {
 	}
 
 	readBytes := readFromTLSConn(tlsConn)
-	fmt.Println(string(readBytes))
+	httpResp := newHTTPResponse(string(readBytes))
+
+	fmt.Printf("%+v\n", httpResp)
 }
