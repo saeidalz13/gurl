aa0c5a30905922804767c73d3b24d62c202abdd0
Saeid Alizadeh <saeidalz96@gmail.com>
2024-10-21 21:15:29 -0600

    modified tcp reading to account for keep-alive with Content-Length

--- api/tcp_tls.go
+++ api/tcp_tls.go
@@ -1,10 +1,12 @@
 package api
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -12,30 +14,125 @@ import (
 	"github.com/saeidalz13/gurl/internal/httpconstants"
 )
 
+const (
+	HeaderCloseConn uint8 = iota
+	HeaderChunk
+	HeaderContentLength
+)
+
 func writeToTLSConn(tlsConn *tls.Conn, b []byte) error {
 	_, err := tlsConn.Write(b)
 	return err
 }
 
+// Identifies which parameter exists in the
+// http response header so we know if we should
+// close the connection right away or keep it
+// alive and handle the rest of streams of data.
+func identifyHeaderParam(bufContainingHeader []byte) uint8 {
+	if bytes.Contains(bufContainingHeader, []byte("Content-Length")) {
+		return HeaderContentLength
+	}
+
+	if bytes.Contains(bufContainingHeader, []byte("chunked")) {
+		return HeaderChunk
+	}
+
+	return HeaderCloseConn
+}
+
+// in HTTP 1.1 the default header setting of
+// connection is "keep-alive".
+//
+// It is the case that if the server wants to send
+// the response in different streams of tcp, it
+// indiciates either "Content-Length" or sets the
+// "Transfer-Encoding" to "chunked"
+//
+// If none of those options provided, that means the
+// entire data is sent in one single stream and we can
+// close the TCP conn after the first read.
 func readFromTLSConn(tlsConn *tls.Conn) []byte {
-	buf := make([]byte, 2<<12)
+	var response bytes.Buffer
+	var readContentLength int = -1
+	var contentLength int
+	var headerParam uint8
 
-	n, err := tlsConn.Read(buf)
-	if err != nil {
-		if err.Error() == "EOF" {
-			os.Exit(0)
-		}
+	bufSize := 2 << 12 // 4kb
+	var readIteration int
+	headerIteration := 1
 
-		if strings.Contains(err.Error(), "i/o timeout") {
-			fmt.Println(err)
+tcpLoop:
+	for readContentLength < contentLength {
+		readIteration++
+		buf := make([]byte, bufSize)
+
+		n, err := tlsConn.Read(buf)
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+
+			if strings.Contains(err.Error(), "i/o timeout") {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			fmt.Printf("error read: %v\n", err)
 			os.Exit(1)
 		}
 
-		fmt.Printf("error read: %v\n", err)
-		os.Exit(1)
+		response.Write(buf[:n])
+
+		if readIteration == headerIteration {
+			headerParam = identifyHeaderParam(buf[:n])
+		}
+
+		switch headerParam {
+		case HeaderCloseConn:
+			return response.Bytes()
+
+		case HeaderContentLength:
+			if readIteration == headerIteration {
+				var bodyPos, shouldBreakNum int
+				bytesLines := bytes.Split(buf[:n], []byte("\r\n"))
+			lineLoop:
+				for _, line := range bytesLines {
+					// + 2 is for \n and \r
+					bodyPos = bodyPos + len(line) + 2
+					if string(line) == "" {
+						shouldBreakNum++
+					}
+
+					if bytes.Contains(line, []byte("Content-Length")) {
+						contentLengthBytes := bytes.TrimSpace(bytes.Split(line, []byte(":"))[1])
+
+						num, err := strconv.ParseInt(string(contentLengthBytes), 10, 64)
+						if err != nil {
+							fmt.Println("invalid response from server")
+							os.Exit(1)
+						}
+						contentLength = int(num)
+						shouldBreakNum++
+					}
+
+					if shouldBreakNum == 2 {
+						break lineLoop
+					}
+				}
+				readContentLength = n - bodyPos
+
+				continue tcpLoop
+			} else {
+				readContentLength += n
+			}
+
+		case HeaderChunk:
+			// TODO: to be implemented
+		}
 	}
 
-	return buf[:n]
+	return response.Bytes()
 }
 
 func mustPrepareCertPool() *x509.CertPool {
