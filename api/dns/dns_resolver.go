4007ef9266f0b7a59c080ff5567dd85285c2e979
Saeid Alizadeh <saeidalz96@gmail.com>
2024-11-05 13:55:19 -0700

    minor structure change

--- api/dns/dns_resolver.go
+++ api/dns/dns_resolver.go
@@ -6,22 +6,10 @@ import (
 	"github.com/saeidalz13/gurl/internal/errutils"
 )
 
-type DNSResolver struct {
-	domainSegments []string
-}
-
-func NewDNSResolver(domainSegments []string) DNSResolver {
-	return DNSResolver{
-		domainSegments: domainSegments,
-	}
-}
-
 // Fetch the domain IPv4 from 8.8.8.8 (Google server).
 // Average time is 25 ms.
-func (dr DNSResolver) MustResolveIP() net.IP {
-	query := NewDNSQueryManager(dr.domainSegments).CreateClassAQuery()
-	// dnsQuery, err := dr.createDNSQuery()
-	// errutils.CheckErr(err)
+func MustResolveIP(domainSegments []string) net.IP {
+	query := NewDNSQueryManager(domainSegments).CreateQuery()
 
 	udpConn, err := net.DialUDP("udp", nil, &net.UDPAddr{Port: 53, IP: net.IPv4(8, 8, 8, 8)})
 	errutils.CheckErr(err)
@@ -40,35 +28,4 @@ func (dr DNSResolver) MustResolveIP() net.IP {
 	errutils.CheckErr(err)
 
 	return ip
-
-	// This shows if Answer section exists
-	// 	anCount := binary.BigEndian.Uint16(response[6:8])
-	// 	if anCount == 0 {
-	// 		log.Fatalln("no answer received from 8.8.8.8 server")
-	// 	}
-
-	// 	pos := 12
-	// 	endQNameValue := 0
-	// 	for int(response[pos]) != endQNameValue {
-	// 		// Move by length of each label + 1 for the length byte
-	// 		// Each label prefixed by a length byte, followed by the label itself
-	// 		// e.g: A domain name consists of labels (e.g., www, example, com in www.example.com).
-	// 		lenghtOfBytes := int(response[pos]) // value of length byte
-	// 		pos += lenghtOfBytes + 1
-	// 	}
-	// 	pos += 1 // End of domain 0x00
-	// 	pos += 4 // QType 2 + QClass 2
-
-	// 	// Now starting Answer section
-	// 	// Name (variable), Type 2, Class 2, TTL 4, Data length 2
-	// 	pos += 12
-	// 	dataLen := binary.BigEndian.Uint16(response[pos-2 : pos])
-
-	// 	if dataLen != 4 {
-	// 		log.Fatalln("invalid ip address received from dns - must be ipv4")
-	// 	}
-
-	//		ip := net.IPv4(response[pos], response[pos+1], response[pos+2], response[pos+3])
-	//		return ip
-	//	}
 }
