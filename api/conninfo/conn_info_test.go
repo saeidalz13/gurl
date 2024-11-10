1c5b6c1b6296e373bb7cd6ad571eb9dc8d49977e
Saeid Alizadeh <saeidalz96@gmail.com>
2024-11-10 15:48:31 -0700

    added extract port unit test

--- api/conninfo/conn_info_test.go
+++ api/conninfo/conn_info_test.go
@@ -1,6 +1,8 @@
 package conninfo
 
-import "testing"
+import (
+	"testing"
+)
 
 var testCir = ConnInfoResolver{}
 
@@ -24,6 +26,35 @@ func TestIsDomainLocalhost(t *testing.T) {
 			resp := testCir.isDomainLocalHost()
 			if resp != test.expectedRes {
 				t.Fatalf("expected: %v\tgot:%v", test.expectedRes, resp)
+			}
+		})
+	}
+}
+
+func TestExtractPort(t *testing.T) {
+	tests := []struct {
+		name        string
+		domain      string
+		expectedRes int
+		expectedErr string
+	}{
+		{name: "should_give_correct_port", domain: "127.0.0.1:1111", expectedRes: 1111},
+		{name: "should_give_correct_port", domain: "127.0.0.1:9999", expectedRes: 9999},
+		{name: "should_give_correct_port", domain: "127.0.0.1", expectedRes: 0, expectedErr: "domain must be in format of ip:port"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			testCir.domain = test.domain
+
+			res, err := testCir.extractPort()
+
+			if test.expectedRes != res {
+				t.Fatalf("expected result: %d\tgot: %d", test.expectedRes, res)
+			}
+
+			if err != nil && test.expectedErr != err.Error() {
+				t.Fatalf("expected err: %s\tgot: %s", test.expectedErr, err.Error())
 			}
 		})
 	}
